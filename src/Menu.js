c533700ce4475427653bbfac364d926fc27b6c4e
Tom Cornyn <cornyn@gmail.com>
2023-10-23 15:38:21 -0700

    Remove Research from nav. Start with a clean Read Me file.

--- src/Menu.js
+++ src/Menu.js
@@ -17,9 +17,6 @@ function Menu() {
                 <Nav.Link href={"/about"}>About</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href={"/research"}>Research</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
                 <Nav.Link href={"/development"}>Development</Nav.Link>
               </Nav.Item>
               <Nav.Item>
