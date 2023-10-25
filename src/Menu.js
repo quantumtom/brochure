db7dc4ca640c35b91ed931f5c5c259f0898a46f0
Tom Cornyn <cornyn@gmail.com>
2023-10-25 14:13:09 -0700

    Add button for dark mode. Add new npm script to serve production builds.

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,21 @@
 import './Menu.css';
+import { useState, useEffect, useReducer } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
 function Menu() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  let newTheme = 'dark';
+
+  useEffect(() => {
+    if (!darkMode) {
+      document.getElementsByTagName('html')[0].setAttribute('data-bs-theme','light');
+    } else {
+      document.getElementsByTagName('html')[0].setAttribute('data-bs-theme','dark');    }
+  }, [darkMode]);
+
   return (
       <Navbar expand={"md"} className={"bg-body-tertiary navigation-menu"}>
         <Container>
@@ -24,6 +36,14 @@ function Menu() {
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link href={"/contact"}>Contact</Nav.Link>
+              </Nav.Item>
+              <Nav.Item className={'dropdown'}>
+                <button className={`btn btn-primary btn-sm text-capitalize`} onClick={
+                  () => {
+                    setDarkMode(!darkMode);
+                  }
+                }
+                >{darkMode} mode</button>
               </Nav.Item>
             </Nav>
           </Navbar.Collapse>
